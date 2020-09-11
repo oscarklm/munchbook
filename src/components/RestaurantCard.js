df8ce869ea3df6b04387d2891923e37e8e97d19b
Oscar Landmark <oscar@landmark.dk>
2020-09-11 15:04:32 +0200

    improved dashboard design and added webp images

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function RestaurantCard({ restaurant }) {
   const { name, location, desc, url, tags } = restaurant;
   return (
-    <div className="flex-none w-full mt-8 shadow-lg sm:flex">
+    <div className="flex-none w-full mb-5 shadow-lg sm:flex">
       <img
         className="flex-none object-cover object-center w-full h-48 sm:h-auto sm:w-48"
         src={url}
@@ -11,29 +11,33 @@ export default function RestaurantCard({ restaurant }) {
       ></img>
       <div className="flex flex-col justify-between p-4 leading-normal bg-white border-b border-l border-r border-gray-400 rounded-b lg:border-l-0 lg:border-t lg:border-gray-400 lg:rounded-b-none lg:rounded-r">
         <div className="mb-8">
-          <div className="text-xl font-bold text-gray-900 ">{name}</div>
+          <div className="text-xl font-bold text-gray-900 sm:text-2xl">
+            {name}
+          </div>
           <div className="mb-2 text-sm font-bold text-gray-500">
             <span className="text-base font-thin ">located in </span>
             {location}
           </div>
           <p className="text-base text-gray-700">{desc}</p>
         </div>
-        <div class="pt-4 pb-2">
-          {tags.map((tag) => (
-            <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              #{tag}
-            </span>
-          ))}
-        </div>
-        <div className="my-4 ml-2">
-          <a
-            className="p-3 mr-2 text-base font-bold tracking-wider text-white uppercase bg-gray-800"
-            onClick={(e) => {
-              e.preventDefault();
-            }}
-          >
-            Book a table
-          </a>
+        <div className="items-center justify-center flex-none sm:flex">
+          <div class=" flex-1 items-center justify-center">
+            {tags.map((tag) => (
+              <span class="inline-block bg-gray-200 rounded-full p-2 text-sm font-semibold text-gray-700 mx-1">
+                #{tag}
+              </span>
+            ))}
+          </div>
+          <div className="mt-6 mb-2 text-center sm:mt-0 sm:mb-0">
+            <a
+              className="p-3 text-sm font-bold tracking-wider text-orange-100 uppercase bg-orange-400 shadow-lg cursor-pointer hover:bg-gray-700"
+              onClick={(e) => {
+                e.preventDefault();
+              }}
+            >
+              Read more...
+            </a>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,20 +4,31 @@ import RestaurantCard from '../components/RestaurantCard';
 export default function index() {
   return (
     <>
-      <div>
-        <h1 className="pt-8 text-3xl font-black text-center sm:text-5xl ">
-          TheMunchBook
+      <div className="flex flex-col items-center justify-center pt-8">
+        <h1 className="text-3xl font-black text-center sm:text-5xl">
+          TheMunchBook 🍔
         </h1>
+        <p className="text-sm font-extrabold text-center text-gray-500 sm:text-base">
+          The{' '}
+          <span className="text-gray-700 border-b-2 border-gray-700 ">
+            {'FASTEST'}
+          </span>{' '}
+          way to find <br className="sm:hidden" />
+          delicious foods, and drinks near you.
+        </p>
       </div>
-      <div className="max-w-screen-md px-8 mx-auto">
+      <div className="max-w-screen-md px-8 mx-auto mt-5 sm:mt-10">
+        <h1 className="my-2 text-lg italic font-extrabold text-gray-300 sm:text-2xl">
+          Restaurants near you...
+        </h1>
         <RestaurantCard
           restaurant={{
             name: 'Ssam Korean Food',
             location: 'Vesterbro',
             desc:
               'Delicious korean food, and nice sparkly cocktails. Also a nice place to listen to some k-pop and drink soju with friends!',
-            url: './img/restaurants/ssam.jpg',
-            tags: ['korean', 'food', 'bbq']
+            url: './img/restaurants/ssambar.webp',
+            tags: ['Korean', 'Food', 'BBQ']
           }}
         />
         <RestaurantCard
@@ -26,8 +37,28 @@ export default function index() {
             location: 'Østerbro',
             desc:
               'Amazing coffee in Østerbro, made with in-house roasted coffee beans freshly imported with love.',
-            url: './img/restaurants/april.png',
-            tags: ['coffee', 'roastery', 'tools']
+            url: './img/restaurants/aprilcoffee.webp',
+            tags: ['Coffee', 'Roastery', 'Tools']
+          }}
+        />
+        <RestaurantCard
+          restaurant={{
+            name: 'Pizza Bro',
+            location: 'Islands Brygge',
+            desc:
+              'Real nice italian Pizza made in stone oven, it crazy what comes out of this little pizza truck.',
+            url: './img/restaurants/pizzabro.webp',
+            tags: ['Pizza', 'Italian', 'StoneOven']
+          }}
+        />
+        <RestaurantCard
+          restaurant={{
+            name: 'Anderson Bakery',
+            location: 'Islands Brygge',
+            desc:
+              'This cozy bakery located in Islands Brygge is one of the best, if not the best in Denmark. Freshly baked bread, and beautiful cakes.',
+            url: './img/restaurants/bakery.webp',
+            tags: ['Pizza', 'Italian', 'StoneOven']
           }}
         />
       </div>
